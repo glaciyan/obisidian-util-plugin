eb7e089ac35c6881a75b7775b57975272061a1c6
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-07-08 13:38:59 +0200

    setup

---
+++ package.sh
@@ -0,0 +1,8 @@
+npm run build
+mkdir -p ./out/kevins-utilities
+mv main.js ./out/kevins-utilities
+
+cp src/manifest.json src/styles.css ./out/kevins-utilities
+
+cp -r out/kevins-utilities "C:\Users\kevin\Downloads\AIN Notizen\.obsidian\plugins"
+echo Installed into "C:\Users\kevin\Downloads\AIN Notizen\.obsidian\plugins"
