bc83b87e39c23026603e150fc0f2dac279b76e79
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-07-08 13:52:13 +0200

    class structure

---
+++ src/plugin/KevinUtilSettingTab.ts
@@ -0,0 +1,41 @@
+import KevinUtilPlugin from "main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+
+export interface KevinUtilPluginSettings {
+	mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: KevinUtilPluginSettings = {
+	mySetting: "default",
+};
+
+export class KevinUtilSettingTab extends PluginSettingTab {
+	plugin: KevinUtilPlugin;
+
+	constructor(app: App, plugin: KevinUtilPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
+
+		new Setting(containerEl)
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						console.log("Secret: " + value);
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
